ab4e9da0c67ba04c82cd29d47e374ac300f1ab8f
Youngjin <yjshin229@gmail.com>
2022-09-05 00:21:20 +0900

    feat: main page setup

---
+++ src/components/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css'
+
+
+function Main(){
+
+    return (
+      <div className='main'>
+          TikiTaka
+      </div>
+    )
+};
+
+export default Main;
