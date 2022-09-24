efa187ffe8baf9cea68845d7952e365d1f526971
Leeseunghwan7305 <sun1238799@naver.com>
2022-09-25 03:18:18 +0900

    음성 ui제작

--- src/components/atom/DownloadApp.jsx
+++ src/components/atom/DownloadApp.jsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const StyledDownload = styled.button`
-    width: 12rem;
-    height: 3rem;
-    margin: 3rem 1rem;
-    padding: 1rem auto;
-    box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
-    background-color: transparent;
-    border-color: black;
-    color: black;
-    font-size: 1rem;
-    border-radius: 1.1rem;
-    font-family: 'Anton';
-    }
+  width: 12rem;
+  height: 3rem;
+  margin: 3rem 1rem;
+  padding: 1rem auto;
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+  background-color: transparent;
+  border-color: black;
+  color: black;
+  font-size: 1rem;
+  border-radius: 1.1rem;
+  font-family: "Anton";
+  position: fixed;
+  bottom: 100px;
+  left: 50%;
+  margin-left: -6rem;
 `;
 
-function DownloadApp  ({disabled,children}){
-    return <StyledDownload disabled={disabled}>{children}
-    </StyledDownload>;
-  }
-  
+function DownloadApp({ disabled, children }) {
+  return <StyledDownload disabled={disabled}>{children}</StyledDownload>;
+}
+
 export default DownloadApp;
--- src/components/box/MessageBox.jsx
+++ src/components/box/MessageBox.jsx
@@ -1,64 +1,177 @@
-import React, {useState} from 'react';
-import styled from 'styled-components';
-import SendButton from '../atom/SendButton';
-import TextArea from '../atom/TextArea';
+import React, { useState } from "react";
+import styled from "styled-components";
+import SendButton from "../atom/SendButton";
+import TextArea from "../atom/TextArea";
+import { motion, AnimatePresence } from "framer-motion";
+const StyledContainer = styled(motion.div)`
+  margin: 0 auto;
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 1.5rem;
+  width: 24rem;
+  box-shadow: 0 5px 5px 0 rgba(0, 0, 0, 0.25);
+  position: absolute;
+  left: 50%;
+  margin-left: -12rem;
+  top: 50px;
+`;
 
-const StyledContainer = styled.div`
-    margin: 6rem auto;
-    background-color: rgba(255, 255, 255, 0.5);
-    border-radius: 1.5rem;
-    width: 24rem;
+const StyledHeader = styled(motion.div)`
+  display: flex;
+  flex-direction: row;
+  background-color: white;
+  height: 6rem;
+  border-radius: 1.5rem 1.5rem 0rem 0rem;
+  justify-content: center;
+  align-items: center;
+`;
+
+const InfoContainer = styled(motion.div)`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  flex: 6;
+  margin-left: 1.5rem;
+  margin-top: 0.5rem;
+`;
+
+const StyledInfo = styled(motion.div)`
+  font-size: 0.9rem;
+  margin-bottom: 0.5rem;
+`;
+
+const Buttons = styled.div`
+  position: absolute;
+  top: 100px;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: transparent;
+  border: none;
+  & button {
+    width: 150px;
+    color: white;
+    background-color: #00ccff;
+    font-size: 18px;
+    font-weight: 700;
+    height: 50px;
+    border: none;
+    border-radius: 50px;
+    padding: 15px 15px;
+    margin: 15px;
+    cursor: pointer;
     box-shadow: 0 5px 5px 0 rgba(0, 0, 0, 0.25);
-
+  }
 `;
 
-const StyledHeader = styled.div`
-    display: flex;
-    flex-direction: row;
-    background-color: white;
-    height: 6rem;
-    border-radius: 1.5rem 1.5rem 0rem 0rem;
-    justify-content: center;
-    align-items: center;
-`;
+const container = {
+  invisible: (isBack) => ({
+    y: 100,
+    opacity: 0,
+    scale: 0.8,
+  }),
+  visible: {
+    y: 100,
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: "spring",
+      duration: 0.6,
+    },
+  },
+  exit: (isBack) => ({
+    y: 100,
+    opacity: 0,
+    scale: 0.8,
+    transition: { type: "spring", duration: 0.6 },
+  }),
+};
 
-
-
-const InfoContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    flex: 6;
-    margin-left: 1.5rem;
-    margin-top: 0.5rem;
-    
-    
-`;
-
-const StyledInfo = styled.div`
-    font-size: 0.9rem;
-    margin-bottom: 0.5rem;
-`;
-
-
-function MessageBox(){
-    const [input, setInput] = useState({
-        message :''
-    })
-    return(
-        <StyledContainer>
+function MessageBox() {
+  const [input, setInput] = useState({
+    message: "",
+  });
+  const [visible, setVisible] = useState(0); // 0: 코멘트 1: 음성
+  const goComment = () => {
+    setVisible(0);
+  };
+  const goSound = () => {
+    setVisible(1);
+  };
+  return (
+    <>
+      <AnimatePresence mode={"wait"}>
+        {visible == 0 ? (
+          <StyledContainer
+            variants={container}
+            initial="invisible"
+            animate="visible"
+            exit="exit"
+            key={visible}
+          >
             <StyledHeader>
-                <img style={{ marginLeft:"2rem",width:"3.8rem" ,height:"3.8rem", flex:"1"}} src = {process.env.PUBLIC_URL + "/logo192.png"} alt = 'logo'/>
-                <InfoContainer>
-                    <StyledInfo>@tikitaka</StyledInfo>
-                    <StyledInfo>The question goes here.</StyledInfo>
-                </InfoContainer>
+              <img
+                style={{
+                  marginLeft: "2rem",
+                  width: "3.8rem",
+                  height: "3.8rem",
+                  flex: "1",
+                }}
+                src={process.env.PUBLIC_URL + "/logo192.png"}
+                alt="logo"
+              />
+              <InfoContainer>
+                <StyledInfo>@tikitaka</StyledInfo>
+                <StyledInfo>The question goes here.</StyledInfo>
+              </InfoContainer>
             </StyledHeader>
-            <TextArea name="LET ME KNOW WHAT YOU THINK..." type="text" onChange={(e) => setInput({...input,"message":e.target.value})}></TextArea>
+            <TextArea
+              name="LET ME KNOW WHAT YOU THINK..."
+              type="text"
+              onChange={(e) => setInput({ ...input, message: e.target.value })}
+            ></TextArea>
 
             <SendButton>SEND!</SendButton>
-        </StyledContainer>
-    )
-    }
+          </StyledContainer>
+        ) : (
+          <StyledContainer
+            variants={container}
+            initial="invisible"
+            animate="visible"
+            exit="exit"
+            key={visible}
+          >
+            <StyledHeader>
+              <img
+                style={{
+                  marginLeft: "2rem",
+                  width: "3.8rem",
+                  height: "3.8rem",
+                  flex: "1",
+                }}
+                src={process.env.PUBLIC_URL + "/logo192.png"}
+                alt="logo"
+              />
+              <InfoContainer>
+                <div>난 음성이야</div>
+                <StyledInfo>@tikitaka</StyledInfo>
+                <StyledInfo>The question goes here.</StyledInfo>
+              </InfoContainer>
+            </StyledHeader>
+            <TextArea
+              name="LET ME KNOW WHAT YOU THINK..."
+              type="text"
+              onChange={(e) => setInput({ ...input, message: e.target.value })}
+            ></TextArea>
+
+            <SendButton>SEND!</SendButton>
+          </StyledContainer>
+        )}
+      </AnimatePresence>
+      <Buttons>
+        <button onClick={goComment}>코멘트</button>
+        <button onClick={goSound}>음성</button>
+      </Buttons>
+    </>
+  );
+}
 
 export default MessageBox;
