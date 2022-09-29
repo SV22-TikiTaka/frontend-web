355ec93ca2ec24d24ffa0636e543746f8157f9f0
Leeseunghwan7305 <sun1238799@naver.com>
2022-09-29 17:01:29 +0900

    메인페이지 (임시)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import './App.css';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import "./App.css";
 import Message from "./components/wrapper/Message";
+import Main from "./page/Main";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Main />}></Route>
           <Route path="/:insta_id/:questionId" element={<Message />} />
         </Routes>
       </BrowserRouter>
--- src/components/wrapper/Message.jsx
+++ src/components/wrapper/Message.jsx
@@ -1,16 +1,14 @@
-import React from 'react';
-import '../../App.css';
-import DownloadApp from '../atom/DownloadApp';
-import MessageBox from '../box/MessageBox';
+import React from "react";
+import "../../App.css";
+import DownloadApp from "../atom/DownloadApp";
+import MessageBox from "../box/MessageBox";
 
-function Message(){
-
-    return (
-      <div className='Message'>
-          <MessageBox></MessageBox>
-          <DownloadApp>DOWNLOAD APP!</DownloadApp>
-      </div>
-    )
-};
+function Message() {
+  return (
+    <div className="Message">
+      <MessageBox></MessageBox>
+    </div>
+  );
+}
 
 export default Message;
