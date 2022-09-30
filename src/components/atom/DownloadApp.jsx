dd22d5f2be267eeaa24354ebcf0d0240662a2ac9
Leeseunghwan7305 <sun1238799@naver.com>
2022-09-30 21:31:27 +0900

    메인페이지 디자인 수정

--- src/components/atom/DownloadApp.jsx
+++ src/components/atom/DownloadApp.jsx
@@ -2,16 +2,25 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledDownload = styled.button`
-  width: 12rem;
-  height: 3rem;
+  width: 20rem;
+  height: 7rem;
   padding: 1rem auto;
   box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
   background-color: transparent;
   border-color: black;
-  color: black;
-  font-size: 1rem;
+  color: white;
+  font-size: 2rem;
+  border: 6px solid black;
+  background-color: black;
+  position: relative;
+  left: -16px;
+  top: -50px;
+
   border-radius: 1.1rem;
   font-family: SBAggro;
+  :hover {
+    transform: scale(1.03);
+  }
 `;
 
 function DownloadApp({ disabled, children }) {
--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -2,15 +2,25 @@ import React from "react";
 import styled, { keyframes } from "styled-components";
 import DownloadApp from "../components/atom/DownloadApp";
 const Main = () => {
-  //   cons Fadeout = keyframes`
-  //   0% {
-  //     opacity: 0;
-  //   }
-  //   100% {
-  //     opacity: 1;
-  //   }
-  // `;
-
+  const Fadeout = keyframes`
+    0% {
+    transform: translateX(-300px);
+    opacity: 0.1;
+    }
+    100% {
+      transform: translateX(0);
+      opacity: 1;
+    }
+  `;
+  const FadeoutRight = keyframes`
+  0% {
+    transform: translateX(300px);
+    }
+    100% {
+      transform: translateX(0);
+      opacity: 1;
+    }
+  `;
   const View = styled.div`
     width: 100vw;
     height: 100vh;
@@ -20,38 +30,62 @@ const Main = () => {
   `;
   const InformNav = styled.div`
     width: 100%;
-    background-color: red;
+    height: 60px;
+    color: whitesmoke;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #ff6347;
+    font-size: 22px;
+    //#FF6347
     position: fixed;
     top: 0;
   `;
   const CenterBox = styled.div`
     display: flex;
-    width: 50%;
+    width: 65%;
   `;
   const ImgBox = styled.div`
     height: 700px;
-
+    perspective: 800px;
+    margin-right: 70px;
+    display: flex;
+    opacity: 0;
+    animation: ${Fadeout} 1.5s forwards;
     & img {
       width: 300px;
       height: 600px;
+      transform: rotateY(10deg);
+    }
+    & img:last-child {
+      width: 300px;
+      height: 600px;
+      transform: rotateY(-10deg);
     }
   `;
   const IntroduceBox = styled.div`
+    animation: ${FadeoutRight} 3s 1s forwards;
     width: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    opacity: 0;
 
     & h1 {
+      font-size: 48px;
       margin-bottom: 200px;
     }
   `;
   const BottomBox = styled.div`
     position: fixed;
     height: 100px;
-    width: 1000px;
-    bottom: 50px;
+    bottom: 150px;
+  `;
+  const UserImg = styled.div`
+    height: 50px;
+    width: 50px;
+    background-image: url("https://github.com/yjshin229");
   `;
   return (
     <View>
@@ -61,6 +95,7 @@ const Main = () => {
       <CenterBox>
         <ImgBox>
           <img src="img/splash.png"></img>
+          <img src="img/setting.png"></img>
         </ImgBox>
         <IntroduceBox>
           <h1>
@@ -70,9 +105,7 @@ const Main = () => {
           <DownloadApp>DOWNLOAD APP!</DownloadApp>
         </IntroduceBox>
       </CenterBox>
-      <BottomBox>
-        <span>여기가 팀원 깃 사진이나 정보 들어가는곳</span>
-      </BottomBox>
+      <BottomBox></BottomBox>
     </View>
   );
 };
